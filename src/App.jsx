c74af2eff6dc54a947e2ebf8706cbd1f929b43b4
htunlinnkyaw <htunlinnkyawmge@gmail.com>
2025-01-01 19:15:38 +0630

    starter-kit with shadcn-ui

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Button } from './components/ui/button'
+
+const App = () => {
+  return (
+    <div className='flex justify-center items-center h-screen bg-black'>
+      <Button>Shadcn UI</Button>
+    </div>
+  )
+}
+
+export default App
