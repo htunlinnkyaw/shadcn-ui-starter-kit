9d86961d7416f9c7aeaefbbcba52bd92531a79be
htunlinnkyaw <tunlinkyaw4781@gmail.com>
2025-01-08 23:20:12 +0630

    fix: dark-mode

--- src/components/DarkModeToggle.jsx
+++ src/components/DarkModeToggle.jsx
@@ -1,27 +1,26 @@
 import { useState } from "react";
 import { Button } from "./ui/button";
 import { LucideMoon, LucideSun } from "lucide-react";
+import { useEffect } from "react";
 
 const DarkModeToggle = () => {
-  const [isDark, setIsDark] = useState(false);
+  const [darkMode, setDarkMode] = useState(() => {
+    return localStorage.getItem("theme") === "dark" || false;
+  });
 
-  const toggleDarkMode = () => {
-    setIsDark(!isDark);
-    if (isDark) {
-      document.documentElement.classList.add("dark");
-    } else {
-      document.documentElement.classList.remove("dark");
-    }
-  };
+  useEffect(() => {
+    document.body.classList.toggle("dark", darkMode);
+    localStorage.setItem("theme", darkMode ? "dark" : "light");
+  }, [darkMode]);
 
   return (
     <Button
-      onClick={toggleDarkMode}
+      onClick={() => setDarkMode(!darkMode)}
       variant="outline"
       size="icon"
       className="flex bg-transparent rounded-lg text-sky-100 hover:text-sky-100 border-sky-500 hover:bg-sky-800 dark:bg-transparent border-2  dark:text-gray-300 dark:hover:bg-transparent px-4 py-2 active:scale-95  gap-1 items-center"
     >
-      {isDark ? <LucideMoon /> : <LucideSun />}
+      {darkMode ? <LucideMoon /> : <LucideSun />}
     </Button>
   );
 };
